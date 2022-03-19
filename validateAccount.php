aeff356eaa05cfae8d391ba60566b8a6a8243e5d
MiguelR0drigues <96126710+MiguelR0drigues@users.noreply.github.com>
2022-03-19 19:06:06 +0000

    fixed notifications on resend email (token verification notes would also appear)

--- validateAccount.php
+++ validateAccount.php
@@ -16,79 +16,81 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         } else {
             $resend_feedback = "failed";
         }
-    }
-    // Check if email is empty
-    if (empty(trim($_POST["token"]))) {
-        $token_err = "Please enter a token.";
     } else {
-        $token = trim($_POST["token"]);
-    }
+        // Check if email is empty
+        if (empty(trim($_POST["token"]))) {
+            $token_err = "Please enter a token.";
+        } else {
+            $token = trim($_POST["token"]);
+        }
 
-    // Validate token
-    if (empty($token_err)) {
+        // Validate token
+        if (empty($token_err)) {
 
-        // Prepare a select statement
-        $sql = "SELECT id,nome, email, token FROM users WHERE email = ?";
-        if ($stmt = mysqli_prepare($link, $sql)) {
+            // Prepare a select statement
+            $sql = "SELECT id,nome, email, token FROM users WHERE email = ?";
+            if ($stmt = mysqli_prepare($link, $sql)) {
 
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_email);
-            // Set parameters
-            $param_email = $_SESSION["email"];
+                // Bind variables to the prepared statement as parameters
+                mysqli_stmt_bind_param($stmt, "s", $param_email);
+                // Set parameters
+                $param_email = $_SESSION["email"];
 
-            // Attempt to execute the prepared statement
-            if (mysqli_stmt_execute($stmt)) {
-                // Store result
-                mysqli_stmt_store_result($stmt);
+                // Attempt to execute the prepared statement
+                if (mysqli_stmt_execute($stmt)) {
+                    // Store result
+                    mysqli_stmt_store_result($stmt);
 
-                // Check if email exists, if yes then verify token
-                if (mysqli_stmt_num_rows($stmt) == 1) {
-                    // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $id, $nome, $email, $resToken);
-                    if (mysqli_stmt_fetch($stmt)) {
-                        if ($token == $resToken) {
-                            session_destroy(); //destroy last session to start a new one
-                            // token is correct, so start a new session
-                            session_start();
+                    // Check if email exists, if yes then verify token
+                    if (mysqli_stmt_num_rows($stmt) == 1) {
+                        // Bind result variables
+                        mysqli_stmt_bind_result($stmt, $id, $nome, $email, $resToken);
+                        if (mysqli_stmt_fetch($stmt)) {
+                            if ($token == $resToken) {
+                                session_destroy(); //destroy last session to start a new one
+                                // token is correct, so start a new session
+                                session_start();
 
-                            // Store data in session variables
-                            $_SESSION["loggedin"] = true;
-                            $_SESSION["id"] = $id;
-                            $_SESSION["email"] = $email;
-                            $_SESSION["user_name"] = $nome;
-                            $_SESSION["type"] = 1;
+                                // Store data in session variables
+                                $_SESSION["loggedin"] = true;
+                                $_SESSION["id"] = $id;
+                                $_SESSION["email"] = $email;
+                                $_SESSION["user_name"] = $nome;
+                                $_SESSION["type"] = 1;
 
-                            $updateQuery = "UPDATE users SET estado=1,validado=1 WHERE id=?";
-                            if ($stm = mysqli_prepare($link, $updateQuery)) {
-                                // Bind variables to the prepared statement as parameters
-                                mysqli_stmt_bind_param($stm, "i", $_SESSION["id"]);
-                                if (mysqli_stmt_execute($stm)) {
-                                    $_SESSION["status"] = 1;
-                                    $_SESSION["validated"] = 1;
-                                    // Redirect user to welcome page
-                                    header("location: index.php");
+                                $updateQuery = "UPDATE users SET estado=1,validado=1 WHERE id=?";
+                                if ($stm = mysqli_prepare($link, $updateQuery)) {
+                                    // Bind variables to the prepared statement as parameters
+                                    mysqli_stmt_bind_param($stm, "i", $_SESSION["id"]);
+                                    if (mysqli_stmt_execute($stm)) {
+                                        $_SESSION["status"] = 1;
+                                        $_SESSION["validated"] = 1;
+                                        // Redirect user to welcome page
+                                        header("location: index.php");
+                                    } else {
+                                        echo "QUERY(UPDATE):::::::Oops! Something went wrong. Please try again later.";
+                                    }
                                 } else {
-                                    echo "QUERY(UPDATE):::::::Oops! Something went wrong. Please try again later.";
+                                    echo mysqli_error($link);
                                 }
                             } else {
-                                echo mysqli_error($link);
+                                // Token is not valid, display a generic error message
+                                $token_err = "Invalid token.";
                             }
-                        } else {
-                            // Token is not valid, display a generic error message
-                            $token_err = "Invalid token.";
                         }
                     }
                 }
+            } else {
+                echo "QUERY(SELECT):::::::Oops! Something went wrong. Please try again later.";
             }
-        } else {
-            echo "QUERY(SELECT):::::::Oops! Something went wrong. Please try again later.";
-        }
 
-        // Close statement
-        mysqli_stmt_close($stmt);
-    } else {
-        echo mysqli_error($link);
-    } //echo "PREPARE STATEMENT::::::::::Oops! Something went wrong. Please try again later.";
+            // Close statement
+            mysqli_stmt_close($stmt);
+        } else {
+            echo mysqli_error($link);
+        } //echo "PREPARE STATEMENT::::::::::Oops! Something went wrong. Please try again later.";
+    }
+
 }
 
 ?>
