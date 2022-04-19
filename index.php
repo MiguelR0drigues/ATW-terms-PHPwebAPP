23de605a9d949e9841f4955e0a39962f23b12160
Telmo Santos <telmo_escapaes@hotmail.com>
2022-04-19 16:53:14 +0100

    alterações

--- index.php
+++ index.php
@@ -81,35 +81,71 @@ isAccountReady();
                     </div>
                </div>
           </section>
-          <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["user_name"] ?? "demo"); ?></b>. Welcome to our site.</h1>
-    <p>
-        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
-        <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
-    </p>
-    <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
-        <button class="button open-button">open modal</button> 
-    
-                <div>
-                <?php if(isAdmin($_SESSION["type"])){ 
+          <?php
+    if(isset($_GET["term_err"]) && $_GET["term_err"]==1){
+        echo '<div class="alert alert-warning" role="alert">';
+        echo 'This term already exists.';
+        echo '</div>';
+    }
+
+    ?>
+       <button class="open-button btn btn-dark">open modal</button>
+        <?php if(isAdmin($_SESSION["type"])){
             echo '<a href="test.php" class="btn btn-primary ml-3">ADMIN</a>';
-      }
+        }
         ?>
-        <dialog class="modal" id="modal">
-        <div class=modal-header>
-            <h1>Insert a term</h1>
+
+        <div>
+        <?php echo $deleteMsg??''; ?>
+        <?php
+            if(is_array($fetchData)){      
+            $sn=1;
+            foreach($fetchData as $data){
+            ?>
+            <div class="card" style="width: 30rem;">
+                <div class="card-header"  style="display: flex; justify-content: space-between;">
+                    <p><a href="term.php?id=<?php echo $data['id']??''; ?>" > <?php echo $data['title']??''; ?></a></p>
+                    <p style="text-align: end;"><?php echo (ownerNameByID($data['owner'],$link))??'Unknown'; ?></p>
+                </div> 
+                <div class="card-body">
+                    <p class="card-text"><?php echo $data['description']??''; ?></p>
+                <div style="display: flex; justify-content: space-between; margin-top:3rem">
+                    <h6 class="" style="text-align: end; font-size:small;color:grey;"><?php echo $data['pubDate']??''; ?></h6>
+                    <?php
+                    if(isAdmin($_SESSION["type"])|| isOwner($_SESSION["id"], $data["id"],$link)){
+                        echo '<div>';
+                        echo '<a href="#" class="card-link" style="font-size:small;">Edit</a>';
+                        echo '<a href="deleteTerm.php?id=',$data["id"],'" class="card-link" style="font-size:small;">Delete</a>';
+                        echo '</div>';   
+                    }
+                    ?>
+                </div>
+                </div>
+            </div><br>
+            <?php
+            $sn++;}}else{ ?>
+            <?php echo $fetchData; ?>
+            <?php
+            }?>
         </div>
-        <form class="form" method="dialog">
-        <label>Title</label>
-        <input type="text" id="title">
-        <label>Description</label>
-        <input type="text" id="description" placeholder="(Max 140 characters)" maxlength="140">
-        <input type="hidden" id="user-id" name="userId" value="<?php echo $_SESSION["id"]?>">
-        <div class="btn2-group">
-            <button class="button" id="submitForm" type="submit">submit form</button>
-            <button class="close button"><b>Close</b></button>
+
+        <dialog class="Termsmodal" id="Termsmodal">
+            <div class=Termsmodal-header>
+                <h1>Insert a term</h1>
+            </div>
+            <form class="form" method="dialog">
+                <label>Title</label>
+                <input type="text" id="title" maxlength="100">
+                <label>Description</label>
+                <input type="text" id="description" placeholder="(Max 140 characters)" maxlength="140">
+                <input type="hidden" id="user-id" name="userId" value="<?php echo $_SESSION["id"]?>">
+                <div class="btn2-group">
+                    <button class="button" id="submitForm" type="submit">submit form</button>
+                    <button class="close button"><b>Close</b></button>
+                </div>
             </form>
-            </dialog>
- 
+        </dialog>
+    </p>
      </div>
      </main>
          <!-- SCRIPTS -->
--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,4 @@
-const modal = document.querySelector("#modal");
+const modal = document.querySelector("#Termsmodal");
 const openModal = document.querySelector(".open-button");
 const closeModal = document.querySelector(".close");
 const submitForm = document.querySelector("#submitForm");
@@ -14,10 +14,15 @@ closeModal.addEventListener("click", () => {
 });
 
 submitForm.addEventListener("click", () => {
+
     var title = document.getElementById("title").value;
     var desc = document.getElementById("description").value;
     if (title && desc) {
-        window.location.replace("insertTermsAction.php/?id=" + userID + "&title=" + title + "&description=" + desc);
+        if ((/^(?:\b\w+\b[\s\r\n]*){1,2}$/.test(title))) {
+            window.location.replace("insertTermsAction.php?id=" + userID + "&title=" + title + "&description=" + desc);
+        } else {
+            alert("Title can have no more than 2 words!");
+        }
     } else {
         alert("Please fill everything up!");
     }
